467b58db33e2ee9b598f428986765526eed82fcd
Glenn Block <gblock@microsoft.com>
2012-05-21 00:37:51 +0200

    another commit

--- apps/hello/server.js
+++ apps/hello/server.js
@@ -1,5 +1,5 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('No frakkin way!\n');
+  res.end('Awesome!\n');
 }).listen(process.env.PORT);
